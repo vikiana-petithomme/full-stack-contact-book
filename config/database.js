f5e7483aba9709371cad1e23570b624fd43fac1c
vikiana-petithomme <113193304+vikiana-petithomme@users.noreply.github.com>
2022-11-06 23:45:07 -0500

    inital commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://vikiana:a123b@cluster0.6ozuol9.mongodb.net/contactBook?retryWrites=true&w=majority', 
+    'dbName': 'contactBook'
+};
